78bc572f8cf1886f21934b91821d8502ebe1e2c4
tu201906115 <tu201906115@214-3>
2021-11-13 12:14:33 +0400

    Add project files.

---
+++ 13112021/Generic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13112021
+{
+    class Generic<Tname,Tphone>
+    {
+        Tphone[] _phones = new Tphone[0];
+        Tname[] _names = new Tname[0];
+       public void Add(Tname name, Tphone phone)
+        {
+            Array.Resize(ref _phones, _phones.Length + 1);
+            _phones[_phones.Length - 1] = phone;
+
+            Array.Resize(ref _names, _names.Length + 1);
+            _names[_names.Length - 1] = name;
+        }
+
+
+        public Tphone[] GetTphone(Tname name)
+        {
+            Tphone[] neededPhones = new Tphone[0];
+            for (int i = 0; i < _names.Length; i++)
+            {
+                if (_names[i].Equals(name))
+                {
+                    Array.Resize(ref neededPhones, neededPhones.Length + 1);
+                    neededPhones[neededPhones.Length - 1] = _phones[i];
+                }
+            }
+            return _phones;
+        }
+
+
+        public Tname Getname(Tphone phone)
+        {
+            for (int i = 0; i < _phones.Length; i++)
+            {
+                if (_phones[i].Equals(phone))
+                {
+                    return _names[i];
+                }
+            }
+            throw new Exception("Bele bir nomre yoxdur");
+        }
+        
+    }
+}
---
+++ 13112021/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _13112021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Generic<string, string> generics = new Generic<string, string>();
+            generics.Add("Elnur", "055");
+            generics.Add("Gulay", "077");
+            generics.Add("Fatime", "010");
+
+            Console.WriteLine(generics.Getname("055"));  
+        }
+    }
+}
